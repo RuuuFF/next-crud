4b23ba4cd413d0be7655feaceb36e77e6aba2e6d
Carlos Daniel <ruuufffaria@gmail.com>
2022-03-14 17:42:49 -0300

    Aula 106. Componentes Layout e Título

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,15 @@
+import Layout from "../components/Layout"
+
 export default function Home() {
   return (
-    <div className=""></div>
+    <div className="
+      flex justify-center items-center h-screen
+      bg-gradient-to-r from-blue-500 to-purple-500
+      text-white
+    ">
+      <Layout title="Cadastro Simples">
+        <span>Conteúdo</span>
+      </Layout>
+    </div>
   )
 }
