4b6eebf3c5bdac0f7e35e30b4e15f214c3cb68ea
Carlos Daniel <ruuufffaria@gmail.com>
2022-03-18 12:27:39 -0300

    Aula 111. Componente Botão

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Button from "../components/Button"
 import Layout from "../components/Layout"
 import Table from "../components/Table"
 import Client from "../core/Client"
@@ -25,6 +26,10 @@ export default function Home() {
       text-white
     ">
       <Layout title="Cadastro Simples">
+        <div className="flex justify-end">
+          <Button color="green" className="mb-4">Novo Cliente</Button>
+        </div>
+
         <Table
           clients={clients}
           selectedClient={selectedClient}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-  ],
+  purge: {
+    content: [
+      "./src/pages/**/*.{js,ts,jsx,tsx}",
+      "./src/components/**/*.{js,ts,jsx,tsx}",
+    ],
+    safelist: [
+      'from-gray-400',
+      'to-gray-700',
+      'from-blue-400',
+      'to-blue-700',
+      'from-green-400',
+      'to-green-700',
+    ]
+  },
   theme: {
     extend: {},
   },
