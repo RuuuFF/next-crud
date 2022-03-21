ff18ed507271c03b553dbe786c0a040d0f1ce308
Carlos Daniel <ruuufffaria@gmail.com>
2022-03-21 14:46:16 -0300

    Aula 115. Configurando Firebase no Projeto

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,16 @@
 module.exports = {
-  purge: {
-    content: [
-      "./src/pages/**/*.{js,ts,jsx,tsx}",
-      "./src/components/**/*.{js,ts,jsx,tsx}",
-    ],
-    safelist: [
-      'from-gray-400',
-      'to-gray-700',
-      'from-blue-400',
-      'to-blue-700',
-      'from-green-400',
-      'to-green-700',
-    ]
-  },
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  safelist: [
+    'from-gray-400',
+    'to-gray-700',
+    'from-blue-400',
+    'to-blue-700',
+    'from-green-400',
+    'to-green-700',
+  ],
   theme: {
     extend: {},
   },
